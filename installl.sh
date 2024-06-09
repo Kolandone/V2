a242c4badbf4b177f298b07b6e83b9ce2513965f
Kolandone <sjamshid81@gmail.com>
2024-06-09 17:35:30 +0330

    Update installl.sh

--- installl.sh
+++ installl.sh
@@ -1,12 +1,7 @@
-#!/bin/bash
-# Run the installr.sh script
-bash <(curl -fsSL https://raw.githubusercontent.com/Ptechgithub/warp/main/endip/install.sh)
-
-# Automatically select option 4 (Install wire-g)
-4
-
-# Wait for the installation to complete
-sleep 2
-
-# Run the wire-g command
-wire-g
+#!/usr/bin/expect
+spawn bash -c "bash <(curl -fsSL https://raw.githubusercontent.com/Kolandone/V2/main/installr.sh)"
+expect "Enter your choice:"
+send "4\r"
+expect "Run -- >"
+send "wire-g\r"
+interact
