f0a2d37df2713ca0e41ce55e0565470690294d1d
Kolandone <sjamshid81@gmail.com>
2024-06-09 18:42:00 +0330

    Create ins.sh

---
+++ ins.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Run the installr.sh script and pipe in the option '4' when prompted
+{ echo "4"; echo "wire-g"; } | bash <(curl -fsSL https://raw.githubusercontent.com/Ptechgithub/warp/main/endip/install.sh)
