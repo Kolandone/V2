a1ee6c5cc328b115857e82666e18799d8b2c3116
Kolandone <sjamshid81@gmail.com>
2024-06-09 19:00:11 +0330

    Update ins.sh

--- ins.sh
+++ ins.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# Define the new domain and port, MTU size, and name
+# Define the MTU size and name
 new_mtu="1280"
 new_name="Koland"
 
 # Run the install.sh script and pipe in the option '1' when prompted
 output1=$({ echo "1"; } | bash <(curl -fsSL https://raw.githubusercontent.com/Kolandone/V2/main/installr.sh))
+
+# Extract the IP and port from output1 using grep and awk
+new_domain_port=$(echo "$output1" | grep -oP '(\d{1,3}\.){3}\d{1,3}:\d+' | head -n 1)
 
 # Run the install.sh script again and pipe in the options '4' and 'wire-g' when prompted
 output2=$({ echo "4"; echo "wire-g"; } | bash <(curl -fsSL https://raw.githubusercontent.com/Kolandone/V2/main/installr.sh))
