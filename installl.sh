02b8f0e859dc7b282e32ffc3ca4bfd6de5a3107b
Kolandone <sjamshid81@gmail.com>
2024-06-09 17:32:02 +0330

    Update installl.sh

--- installl.sh
+++ installl.sh
@@ -2,9 +2,6 @@
 # Run the installr.sh script
 bash <(curl -fsSL https://raw.githubusercontent.com/Ptechgithub/warp/main/endip/install.sh)
 
-# Wait for the script to load its options
-sleep 2
-
 # Automatically select option 4 (Install wire-g)
 echo "4" | bash
 
