14485faf2f529dca966a1ee259c38e73353a8d05
Kolandone <sjamshid81@gmail.com>
2024-06-09 18:46:50 +0330

    Update ins.sh

--- ins.sh
+++ ins.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
-# Run the installr.sh script and pipe in the option '4' when prompted
-{ echo "4"; echo "wire-g"; } | bash <(curl -fsSL https://raw.githubusercontent.com/Ptechgithub/warp/main/endip/install.sh)
+
+# Define the new domain and port, MTU size, and name
+new_domain_port="example.com:1234"
+new_mtu="1280"
+new_name="Koland"
+
+# Run the install.sh script and pipe in the option '1' when prompted
+output1=$({ echo "1"; } | bash <(curl -fsSL https://raw.githubusercontent.com/Ptechgithub/warp/main/endip/install.sh))
+
+# Run the install.sh script again and pipe in the options '4' and 'wire-g' when prompted
+output2=$({ echo "4"; echo "wire-g"; } | bash <(curl -fsSL https://raw.githubusercontent.com/Kolandone/V2/main/installr.sh))
+
+# Combine outputs
+output="$output1\n$output2"
+
+# Use sed to replace the strings in the output
+output=$(echo -e "$output" | sed -e "s/engage.cloudflareclient.com:2408/$new_domain_port/g")
+output=$(echo -e "$output" | sed -e "s/1280/$new_mtu/g")
+output=$(echo -e "$output" | sed -e "s/Koland/$new_name/g")
+
+# Display the modified output
+echo -e "$output"
