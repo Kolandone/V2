3e2231eb23394059cefe9079638eb424e3e6ab34
Kolandone <sjamshid81@gmail.com>
2024-06-09 18:50:27 +0330

    Update installr.sh

--- installr.sh
+++ installr.sh
@@ -2,13 +2,3 @@
 
 # Run the install.sh script
 bash <(curl -fsSL https://raw.githubusercontent.com/Ptechgithub/warp/main/endip/install.sh)
-
-# Execute the command '4'
-
-
-# Run the wire-g command and modify the output
-output=$(wire-g)
-modified_output=${output//1420/1280}
-
-# Print the new output
-echo "$modified_output"
