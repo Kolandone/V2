93769f22401d7f21738482ef4f93021fcf70bc82
Kolandone <sjamshid81@gmail.com>
2024-06-09 18:38:10 +0330

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -6,18 +6,18 @@ new_mtu="1280"
 new_name="Koland"
 
 # Run the install.sh script and pipe in the option '1' when prompted
-output1=$({ echo "1"; } | bash <(curl -fsSL https://raw.githubusercontent.com/Ptechgithub/warp/main/endip/install.sh))
+output1=$({ echo "1"; } | bash <(curl -fsSL https://raw.githubusercontent.com/Ptechgithub/warp/main/endip/install.sh) 2>&1)
 
 # Run the install.sh script again and pipe in the options '4' and 'wire-g' when prompted
-output2=$({ echo "4"; echo "wire-g"; } | bash <(curl -fsSL https://raw.githubusercontent.com/Ptechgithub/warp/main/endip/install.sh))
+output2=$({ echo "4"; echo "wire-g"; } | bash <(curl -fsSL https://raw.githubusercontent.com/Ptechgithub/warp/main/endip/install.sh) 2>&1)
 
 # Combine outputs
-output="$output1\n$output2"
+combined_output="$output1\n$output2"
 
 # Use sed to replace the strings in the output
-output=$(echo -e "$output" | sed -e "s/engage.cloudflareclient.com:2408/$new_domain_port/g")
-output=$(echo -e "$output" | sed -e "s/1280/$new_mtu/g")
-output=$(echo -e "$output" | sed -e "s/Koland/$new_name/g")
+modified_output=$(echo -e "$combined_output" | sed -e "s/engage.cloudflareclient.com:2408/$new_domain_port/g")
+modified_output=$(echo -e "$modified_output" | sed -e "s/1280/$new_mtu/g")
+modified_output=$(echo -e "$modified_output" | sed -e "s/Peyman_wire-g/$new_name/g")
 
 # Display the modified output
-echo -e "$output"
+echo -e "$modified_output"
