888ed6c375e1b8c5a987a036de7966d6542fe828
Kolandone <sjamshid81@gmail.com>
2024-06-09 18:39:39 +0330

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-# Define the new domain and port, MTU size, and name
-new_domain_port="example.com:1234"
-new_mtu="1280"
-new_name="Koland"
+# Prompt for user input
+read -p "Enter the new domain and port (e.g., example.com:1234): " new_domain_port
+read -p "Enter the new MTU size (default is 1280): " new_mtu
+read -p "Enter the new name (default is Koland): " new_name
 
-# Run the install.sh script and pipe in the option '1' when prompted
-output1=$({ echo "1"; } | bash <(curl -fsSL https://raw.githubusercontent.com/Ptechgithub/warp/main/endip/install.sh) 2>&1)
+# Set default values if no input is provided
+new_domain_port=${new_domain_port:-"engage.cloudflareclient.com:2408"}
+new_mtu=${new_mtu:-"1280"}
+new_name=${new_name:-"Koland"}
 
-# Run the install.sh script again and pipe in the options '4' and 'wire-g' when prompted
-output2=$({ echo "4"; echo "wire-g"; } | bash <(curl -fsSL https://raw.githubusercontent.com/Ptechgithub/warp/main/endip/install.sh) 2>&1)
-
-# Combine outputs
-combined_output="$output1\n$output2"
+# Run the installr.sh script and pipe in the option '4' when prompted
+output=$({ echo "4"; echo "wire-g"; } | bash <(curl -fsSL https://raw.githubusercontent.com/Ptechgithub/warp/main/endip/install.sh))
 
 # Use sed to replace the strings in the output
-modified_output=$(echo -e "$combined_output" | sed -e "s/engage.cloudflareclient.com:2408/$new_domain_port/g")
-modified_output=$(echo -e "$modified_output" | sed -e "s/1280/$new_mtu/g")
-modified_output=$(echo -e "$modified_output" | sed -e "s/Peyman_wire-g/$new_name/g")
+output=$(echo "$output" | sed -e "s/engage.cloudflareclient.com:2408/$new_domain_port/g")
+output=$(echo "$output" | sed -e "s/1420/$new_mtu/g")
+output=$(echo "$output" | sed -e "s/Peyman_wire-g/$new_name/g")
 
 # Display the modified output
-echo -e "$modified_output"
+echo "$output"
