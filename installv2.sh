811f0f0a8795c9cdd907f32a71c54e0726c1de0d
Kolandone <sjamshid81@gmail.com>
2024-06-10 19:18:50 +0330

    Update installv2.sh

--- installv2.sh
+++ installv2.sh
@@ -1,3 +1,3 @@
 apt update -y
 apt install xz-utils bzip2 -y
-curl -o $PREFIX/bin/kolandone https://raw.githubusercontent.com/Kolandone/V2/main/kolandone.sh
+curl -o bin/kolandone https://raw.githubusercontent.com/Kolandone/V2/main/kolandone.sh
