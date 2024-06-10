31cd1e32a81cdf3720308fe344f1c714b2ab7c63
Kolandone <sjamshid81@gmail.com>
2024-06-10 19:53:07 +0330

    Delete installl.sh

--- installl.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Run the installr.sh script and pipe in the option '4' when prompted
-{ echo "4"; echo "wire-g"; } | bash <(curl -fsSL https://raw.githubusercontent.com/Kolandone/V2/main/installr.sh)
