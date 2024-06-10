b557bf52ddd3af36e1d02ff8afaebf542b818da0
Kolandone <sjamshid81@gmail.com>
2024-06-10 18:37:32 +0330

    Update installv2.sh

--- installv2.sh
+++ installv2.sh
@@ -1,4 +1,4 @@
- apt update -y
-apt install xz-utils bzip2 -y
+apt update -y
+apt install wireguard-tools jq xz-utils bzip2 -y
 curl -o $PREFIX/bin/kolandone https://raw.githubusercontent.com/Kolandone/V2/main/kolandone.sh
-chmod +x $PREFIX/bin/kolandone
+chmod +x $PREFIX/bin/kolanone
