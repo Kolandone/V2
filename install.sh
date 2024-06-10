604ae50e20c95ac5069188a56398fd957dc53dbc
Kolandone <sjamshid81@gmail.com>
2024-06-10 19:52:49 +0330

    Delete install.sh

--- install.sh
+++
@@ -1,4 +0,0 @@
- apt update -y
-apt install wireguard-tools jq xz-utils bzip2 -y
-curl -o $PREFIX/bin/wire-g https://raw.githubusercontent.com/Ptechgithub/warp/main/wire-g.sh
-chmod +x $PREFIX/bin/wire-g
