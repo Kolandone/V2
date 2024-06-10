79fbf5860f7b5e894f8dae5cf10a55dce1f92bfd
Kolandone <sjamshid81@gmail.com>
2024-06-10 18:30:20 +0330

    Update new.sh

--- new.sh
+++ new.sh
@@ -8,6 +8,7 @@ run_wire_g() {
         wire-g
     else
         echo "wire-g command not found."
+        return 1 # Return an error code if wire-g is not found
     fi
 }
 
@@ -18,11 +19,9 @@ read -p "Enter the new name (default is Koland): " new_name
 
 # Check if the user has provided a domain and port
 if [ -z "$user_input_domain_port" ]; then
-    # User did not provide a domain and port; run the install.sh script and select option 1
     echo "No domain and port provided. Fetching from install.sh..."
     fetched_domain_port=$(echo "1" | bash <(curl -fsSL https://raw.githubusercontent.com/Kolandone/V2/main/installr.sh) | grep -oP '(\d{1,3}\.){3}\d{1,3}:\d+' | head -n 1)
     
-    # Check if we got a valid IP and port
     if [ -z "$fetched_domain_port" ]; then
         echo "Failed to fetch a valid IP and port. Exiting..."
         exit 1
@@ -30,7 +29,6 @@ if [ -z "$user_input_domain_port" ]; then
         new_domain_port=$fetched_domain_port
     fi
 else
-    # Use the provided domain and port
     new_domain_port=$user_input_domain_port
 fi
 
@@ -38,16 +36,18 @@ fi
 new_mtu=${new_mtu:-"1280"}
 new_name=${new_name:-"Koland"}
 
-# Run the install.sh script and pipe in the option '4' when prompted
+# Run the install.sh script
 output=$(bash <(curl -fsSL https://raw.githubusercontent.com/Kolandone/V2/main/install.sh))
 
-# After running install.sh, run wire-g
-run_wire_g
+# After running install.sh, run wire-g and check if it was successful
+if run_wire_g; then
+    # Use sed to replace the strings in the output
+    output=$(echo "$output" | sed -e "s/engage.cloudflareclient.com:2408/$new_domain_port/g")
+    output=$(echo "$output" | sed -e "s/1420/$new_mtu/g")
+    output=$(echo "$output" | sed -e "s/Peyman_wire-g/$new_name/g")
 
-# Use sed to replace the strings in the output
-output=$(echo "$output" | sed -e "s/engage.cloudflareclient.com:2408/$new_domain_port/g")
-output=$(echo "$output" | sed -e "s/1420/$new_mtu/g")
-output=$(echo "$output" | sed -e "s/Peyman_wire-g/$new_name/g")
-
-# Display the modified output
-echo "$output"
+    # Display the modified output
+    echo "$output"
+else
+    echo "Failed to run wire-g. Please check if it's installed and in your PATH."
+fi
