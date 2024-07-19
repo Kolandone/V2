aa699194b11e40d66971556021205162c7c7014b
Kolandone <sjamshid81@gmail.com>
2024-07-19 14:21:26 +0330

    Update koland.sh

--- koland.sh
+++ koland.sh
@@ -10,10 +10,10 @@ read -p "Select an option (1 for IPv4, 2 for IPv6): " user_choice
 # Fetch IP address based on user's choice
 if [ "$user_choice" == "1" ]; then
     echo "Fetching IPv4 address from install.sh..."
-    fetched_ip=$(echo "1" | bash <(curl -fsSL https://raw.githubusercontent.com/Kolandone/V2/main/installr.sh) | grep -oP '(\d{1,3}\.){3}\d{1,3}:\d+' | head -n 1)
+    fetched_ip=$(echo "1" | bash <(curl -fsSL https://raw.githubusercontent.com/Kolandone/Selector/main/Sel.sh) | grep -oP '(\d{1,3}\.){3}\d{1,3}:\d+' | head -n 1)
 elif [ "$user_choice" == "2" ]; then
     echo "Fetching IPv6 address from install.sh..."
-    fetched_ip=$(echo "2" | bash <(curl -fsSL https://raw.githubusercontent.com/Kolandone/V2/main/installr.sh) | grep -oP '(\[?[a-fA-F\d:]+\]?\:\d+)' | head -n 1)
+    fetched_ip=$(echo "2" | bash <(curl -fsSL https://raw.githubusercontent.com/Kolandone/Selector/main/Sel.sh) | grep -oP '(\[?[a-fA-F\d:]+\]?\:\d+)' | head -n 1)
 else
     echo "Invalid choice. Exiting..."
     exit 1
@@ -32,13 +32,21 @@ new_mtu=${new_mtu:-"1280"}
 new_name=${new_name:-"Koland"}
 
 # Run the installr.sh script and pipe in the option '4' when prompted
-output=$({ echo "4"; echo "wire-g"; } | bash <(curl -fsSL https://raw.githubusercontent.com/Kolandone/V2/main/installr.sh))
+output=$({ echo "4"; echo "wire-g"; } | bash <(curl -fsSL https://raw.githubusercontent.com/Ptechgithub/warp/main/endip/install.sh))
 
-# Use sed to replace the strings in the output, properly escaping the new domain and port
-escaped_domain_port=$(printf '%s\n' "$new_domain_port" | sed 's/[]\/$*.^|[]/\\&/g')
-output=$(echo "$output" | sed -e "s|engage.cloudflareclient.com:2408|$escaped_domain_port|g")
-output=$(echo "$output" | sed -e "s|1420|$new_mtu|g")
-output=$(echo "$output" | sed -e "s|Peyman_wire-g|$new_name|g")
+# Check if the previous command was successful
+if [ $? -eq 0 ]; then
+    # Run the wire-g command and capture its output
+    wireg_output=$(wire-g)
 
-# Display the modified output
-echo "$output"
+    # Use sed to replace the strings in the wire-g output, properly escaping the new domain and port
+    escaped_domain_port=$(printf '%s\n' "$new_domain_port" | sed 's/[]\/$*.^|[]/\\&/g')
+    wireg_output=$(echo "$wireg_output" | sed -e "s|engage.cloudflareclient.com:2408|$escaped_domain_port|g")
+    wireg_output=$(echo "$wireg_output" | sed -e "s|1420|$new_mtu|g")
+    wireg_output=$(echo "$wireg_output" | sed -e "s|Peyman_wire-g|$new_name|g")
+
+    # Display the modified wire-g output
+    echo "$wireg_output"
+else
+    echo "The initial command failed."
+fi
